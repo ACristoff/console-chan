5c3469a6dad8192845bc842165ce267548fdf268
acristoff <acristoffanini@gmail.com>
2026-03-06 16:46:31 -0800

    react components

---
+++ src/react.ts
@@ -0,0 +1,16 @@
+import  { useEffect } from "react"
+import { logConsoleArt, LogOptions } from "./index"
+
+export function useConsoleArt(options?: LogOptions) {
+  useEffect(() => {
+    logConsoleArt(options)
+  }, [])
+}
+
+
+
+
+export function ConsoleArt (props?: LogOptions) {
+  useConsoleArt(props);
+  return null
+}
